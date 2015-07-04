fbdd860ecbf9b1b4c67a21e27fbd3aac84d2a7c2
Minami1 <minami1@MacBook.local>
2015-07-04 11:45:11 +0900

    Initial Commit

---
+++ Test1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test1
+//
+//  Created by Minami1 on 2015/07/04.
+//  Copyright (c) 2015年 private. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
